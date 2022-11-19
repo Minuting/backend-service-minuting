4d130f328d2a11e7ee819e704e356bf0213bc850
JeongUk Park <dnr1105@gmail.com>
2022-11-19 10:36:37 +0900

    feature: 에러 처리 추가
    - Minutes NotFoundException 메시지 추가
    - `.orElseThrow` 에러 반환시 공통에러로 패키징해서 반환

--- service/src/main/kotlin/net/huray/backend/minuting/service/MinutesReadService.kt
+++ service/src/main/kotlin/net/huray/backend/minuting/service/MinutesReadService.kt
@@ -1,8 +1,9 @@
 package net.huray.backend.minuting.service
 
+import net.huray.backend.http.exception.NotFoundException
 import net.huray.backend.minuting.dto.MinutesDto
 import net.huray.backend.minuting.repository.MinutesRepository
-import org.modelmapper.ModelMapper
+import net.huray.backend.minuting.support.ErrorMessages.MINUTES_NOT_FOUND
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -16,7 +17,7 @@ class MinutesReadService(
         .map { MinutesDto.MinutesSimple(it.id, it.title, it.contents) }
 
     fun getDetailById(id: Long) = minutesRepository.findById(id)
-        .orElseThrow { throw Exception() }
+        .orElseThrow { throw NotFoundException(MINUTES_NOT_FOUND, id) }
         .let {
             MinutesDto.MinutesDetail(it.id, it.title, it.contents).apply {
                 this.createdAt = it.createdAt
--- service/src/main/kotlin/net/huray/backend/minuting/support/ErrorMessages.kt
+++ service/src/main/kotlin/net/huray/backend/minuting/support/ErrorMessages.kt
@@ -1,4 +1,5 @@
 package net.huray.backend.minuting.support
 
 object ErrorMessages {
+    val MINUTES_NOT_FOUND = "minutes.notFount" to "Minutes({0}) not found."
 }
