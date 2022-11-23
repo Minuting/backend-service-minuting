34a33614ba7dd82d79c408d4f1d55c5059e1bc4a
JeongUk Park <dnr1105@gmail.com>
2022-11-23 18:42:24 +0900

    refactor: `decode` 함수에 scope function 추가
    - FeignErrorDecoder 의 `decode` 함수 가독성 및 response 범위 확장

--- service/src/main/kotlin/net/huray/backend/minuting/support/FeignErrorDecoder.kt
+++ service/src/main/kotlin/net/huray/backend/minuting/support/FeignErrorDecoder.kt
@@ -7,9 +7,11 @@ import net.huray.backend.http.exception.BaseException
 import org.springframework.stereotype.Component
 
 @Component
-class FeignErrorDecoder: ErrorDecoder {
-    override fun decode(methodKey: String, response: Response): Exception =
-        if (response.status() >= 500)
-            RetryableException(response.status(), response.reason(), response.request().httpMethod(), null, response.request())
-        else BaseException(response.status(), response.reason())
+class FeignErrorDecoder : ErrorDecoder {
+
+    override fun decode(methodKey: String, response: Response): Exception = with(response) {
+        if (status() >= 500) RetryableException(status(), reason(), request().httpMethod(), null, request())
+        else BaseException(status(), reason())
+    }
+
 }
