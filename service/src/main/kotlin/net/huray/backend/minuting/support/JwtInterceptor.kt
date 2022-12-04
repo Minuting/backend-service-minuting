b84645a7b6fd90842a61f822ff73e8e089f88c21
dohyeon5626 <kodohyeon71@gmail.com>
2022-12-04 21:50:30 +0900

    fix: CORS 문제 해결
    - Options 호출 시 interceptor에서 토큰 검사 과정 제외

--- service/src/main/kotlin/net/huray/backend/minuting/support/JwtInterceptor.kt
+++ service/src/main/kotlin/net/huray/backend/minuting/support/JwtInterceptor.kt
@@ -3,6 +3,7 @@ package net.huray.backend.minuting.support
 import net.huray.backend.http.exception.InvalidTokenException
 import net.huray.backend.minuting.repository.AccountRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -14,15 +15,16 @@ class JwtInterceptor(
 ) : HandlerInterceptor {
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        request.getHeader("Authorization")
-            ?.takeIf { it.startsWith("Bearer ") }
-            ?.let { it.substring(7) }
-            ?.let { jwtProvider.getBody(it) }
-            ?.takeIf { jwtProvider.isAccess(it) }
-            ?.let { jwtProvider.getId(it) }
-            ?.let { accountRepository.findByIdOrNull(it) }
-            ?.apply { authenticationFacade.setInfo(id, email, name) }
-            ?: throw InvalidTokenException(ErrorMessages.INVALID_TOKEN)
+        if (HttpMethod.OPTIONS.matches(request.method))
+            request.getHeader("Authorization")
+                ?.takeIf { it.startsWith("Bearer ") }
+                ?.let { it.substring(7) }
+                ?.let { jwtProvider.getBody(it) }
+                ?.takeIf { jwtProvider.isAccess(it) }
+                ?.let { jwtProvider.getId(it) }
+                ?.let { accountRepository.findByIdOrNull(it) }
+                ?.apply { authenticationFacade.setInfo(id, email, name) }
+                ?: throw InvalidTokenException(ErrorMessages.INVALID_TOKEN)
         return super.preHandle(request, response, handler)
     }
 }
